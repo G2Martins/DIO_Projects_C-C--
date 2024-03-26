4fbc2a4f944c968c35a21e6d8a1146e30ecb7680
G2Martins <gustavogripaldi@outlook.com>
2024-03-26 12:39:57 -0300

    Add

--- Desafios/Projeto Paradigma de Programação/VerificarSaldoAspect.java
+++ Desafios/Projeto Paradigma de Programação/VerificarSaldoAspect.java
@@ -1,11 +1,11 @@
-public aspect VerificarSaldoAspect {
-    pointcut sacarPointcut(Conta conta, double valor) :
-        call(public void Conta.sacar(double)) && target(conta) && args(valor);
+// public aspect VerificarSaldoAspect {
+//     pointcut sacarPointcut(Conta conta, double valor) :
+//         call(public void Conta.sacar(double)) && target(conta) && args(valor);
 
-    before(Conta conta, double valor) : sacarPointcut(conta, valor) {
-        if (conta.getSaldo() < valor) {
-            System.out.println("Saldo insuficiente para sacar " + valor + " da conta " + conta.getTipoConta());
-            // Aqui pode ser implementada uma lógica para lançar uma exceção, por exemplo
-        }
-    }
-}
+//     before(Conta conta, double valor) : sacarPointcut(conta, valor) {
+//         if (conta.getSaldo() < valor) {
+//             System.out.println("Saldo insuficiente para sacar " + valor + " da conta " + conta.getTipoConta());
+//             // Aqui pode ser implementada uma lógica para lançar uma exceção, por exemplo
+//         }
+//     }
+// }
